738d0ed95424d7acc69615a5dbceea425658e53b
Johnny Tai <bm414148@gmail.com>
2023-07-14 02:06:55 +0800

    all set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-scroll'
+import mdi_file from '../image/svg/mdi_file.svg'
+import mdi_tools from '../image/svg/mdi_tools.svg'
+
 export const Hero = () => {
   return (
     <section id="hero" className='hero' >
@@ -10,12 +13,15 @@ export const Hero = () => {
         </div>
         <p className='hero__cta'>
 
-          <span className="cta-btn cta-btn--hero">
-            <Link to="skills" smooth duration={300}>Skills &thinsp;</Link>
+          <span className="cta-btn cta-btn--hero ">
+            <Link to="skills" smooth duration={300} className="hero__link">
+              <img src={mdi_tools} width="30px" height="30px" alt="" /><span>Skills </span></Link>
           </span>
           <a href="https://drive.google.com/file/d/1zFuBk9cyciz2NUc6s10bGola6iiOW-DO/view" target="_blank" >
-            <span className="cta-btn cta-btn--hero">
-              Resume  &thinsp;
+            <span className="cta-btn cta-btn--hero " >
+              <div className="hero__link">
+              <img src={mdi_file} width="30px" height="30px" alt="" /> <span>Resume</span>
+              </div>
             </span>
           </a>
         </p>
